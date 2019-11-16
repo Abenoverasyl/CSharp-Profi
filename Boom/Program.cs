31a1687be517019629b09811d84d930406eae5f8
Yerassyl <abenov.erasyl@gmail.com>
2019-11-16 23:49:14 +0600

    Добавьте файлы проекта.

---
+++ Boom/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static Generics.BookList;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book { Isbn = "123", Title = "title" };
+
+            var numbers = new GenericList<int>();
+            numbers.Add(10);
+
+            var books = new GenericList<Book>();
+            books.Add(new Book());
+
+        }
+    }
+}
